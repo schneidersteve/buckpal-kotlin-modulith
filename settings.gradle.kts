4eafe6581f8ec4f26a36d71c5cb60a67c0657531
Steve Schneider <45122709+schneidersteve@users.noreply.github.com>
2024-04-14 19:23:09 +0200

    Optimize dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,4 @@
 rootProject.name = "buckpal-kotlin-modulith"
 
-//include("domain")
-//include("application")
-//include("adapters-inbound:rest")
-//include("adapters-outbound:persistence")
-
 include("buckpal")
 include("main")
